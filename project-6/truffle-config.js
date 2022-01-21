caf95302b4e3da58016e9b746edf4c881bfbac2e
holger.heymanns@gmail.com <64ewkyg7DG9ylJquZgEQ>
2022-01-21 06:30:40 +0100

    Add possibility to deploy on rinkeby test network

--- project-6/truffle-config.js
+++ project-6/truffle-config.js
@@ -1,3 +1,9 @@
+const HDWallet = require('truffle-hdwallet-provider');
+const infuraKey = 'f650436ce3864bf49d4c456cc7c391a8';
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync('.secret').toString().trim();
+
 module.exports = {
   networks: {
     development: {
@@ -5,6 +11,14 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
       websockets: true
+    },
+    rinkeby: {
+      provider: () => new HDWallet(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     }
   },
   compilers: {
